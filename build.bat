2492d9199c022de0068d589ceaf9d8c9db70e89a
unknown <midnight337767@gmail.com>
2019-05-11 18:36:53 -0400

    build script

---
+++ build.bat
@@ -0,0 +1,11 @@
+make clean
+cd installFinalizer
+make clean
+make
+cp installFinalizer.3dsx ../data/installFinalizer.3dsx
+cd ..
+make firm SCRIPT_RUNNER=1
+rm output/*.sha
+rm output/*_dev.*
+mv output/GodMode9.firm output/cartInstall.firm
+PAUSE
